ad56da1f776bf4dbff67306e5979fabdc0b6093c
miguelsepulveda671 <masepulveda@miners.utep.edu>
2021-10-20 15:41:01 -0600

    First commit

---
+++ src/UI.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "tokennier.h"
+
+void main()
+{
+  printf(">");
+  char input = 0;
+  while(input != '\n')
+    {
+      input = getchar();
+      printf("/.c",input);
+      
+    }
+  
+}
